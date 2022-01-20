b3d7cfd3aaea77ed0d90f34b2fe8e47e8e3275d8
anilsamkin <94707320+anilsamkin@users.noreply.github.com>
2022-01-19 23:40:21 -0800

    Update VMConfigurationImage.ps1

--- VMConfigurationImage.ps1
+++ VMConfigurationImage.ps1
@@ -1,5 +1,2 @@
 Add-WindowsFeature Web-Server
-Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
-New-Item -ItemType directory -Path "C:\inetpub\wwwroot\images"
-$imagevalue = "Images: " + $($env:computername)
-Add-Content -Path "C:\inetpub\wwwroot\images\test.htm" -Value $imagevalue
+Set-Content -Path "C:\inetpub\wwwroot\images.htm" -Value "This is Image Server from host $($env:computername) !"
